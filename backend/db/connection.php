6817046ae9a1b77b240aeec7fb1992cd43aea6fa
SanjeevRae <sanzeeprae@gmail.com>
2025-03-25 17:18:05 +0545

    update

--- backend/db/connection.php
+++ backend/db/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sqlXXX.epizy.com'; 
-$db = 'epiz_12345678';     
-$user = 'epiz_12345678';   
-$pass = 'your_password';   
+$host = 'sql303.infinityfree.com'; 
+$db = 'if0_38598539_what2eat';     
+$user = 'if0_38598539';   
+$pass = 'in9W4XVBoqQ';   
 try {
     // Create a new PDO instance
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
