e0cb365a5813932281c320f62068df4ac54ac554
SanjeevRae <sanzeeprae@gmail.com>
2025-03-25 17:44:07 +0545

    remove duplicate

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,14 +4,14 @@ session_start();
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="index.php">What2Eat</a>
+        <a class="navbar-brand" href="../index.php">What2Eat</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="menu.php">Menu</a>
